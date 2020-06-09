46b0718736cde01a5c0692c811a8195b5494c42c
rei0531 <poripuropiren12@gmail.com>
2020-06-09 11:18:14 +0900

    新規作成

---
+++ U-22Team2/Player.cpp
@@ -0,0 +1,16 @@
+#include "DxLib.h"
+
+//プログラムはWinMainから始まります
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	ChangeWindowMode(TRUE);
+	if (DxLib_Init() == -1)  return -1;		//DXライブラリ初期化処理
+
+	DrawPixel(320, 240, 0xffffff);  //点を打つ
+
+	WaitKey();					//キー入力待ち
+
+	DxLib_End();				//DXライブラリ使用の終了処理
+
+	return 0;					//ソフトの終了
+}
