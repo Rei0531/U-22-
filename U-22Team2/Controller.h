f4e8c1dbbbba8c738217dde6651e862cc0b6b71e
rei0531 <poripuropiren12@gmail.com>
2020-06-12 15:09:06 +0900

    マップスクロール処理

--- U-22Team2/Controller.h
+++ U-22Team2/Controller.h
@@ -1,3 +1,10 @@
 #pragma once
+#include "DxLib.h"
 
-void a(void); 
+struct Controller {
+public:
+	int NowKey  = GetJoypadInputState(DX_INPUT_KEY_PAD1);;		//現在のキー情報
+	int OldKey = NowKey;		//前のキーの情報
+	int KeyFlg = NowKey & ~OldKey;	//キーフラグ
+};
+void ControllerVlue(void);
--- U-22Team2/Map.cpp
+++ U-22Team2/Map.cpp
@@ -1,12 +1,14 @@
 #include "DxLib.h"
 #include "Map.h"
 
+extern MapCoordinate MapC;
+MapCoordinate MapC;
 image g_pic;
 
 int Map(void) {
 	DrawBox(0, 0, 1280, 768, 0xffffff, TRUE);//白い四角
 	//DrawRotaGraph(1280/2, 768/2,1.0,0,g_pic.Map,TRUE,FALSE);//マップ画像の描画
-	DrawExtendGraph(0, -1536, 5120, 768, g_pic.Map, TRUE);
+	DrawExtendGraph(MapC.X1, MapC.Y1, MapC.X2, MapC.Y2, g_pic.Map, TRUE);
 	//DrawPixel(320, 240, 0xffffff);  //点を打つ
 	return 0;
 }
