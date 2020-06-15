92190a46f65ef269fdf7769a7113987af72f0d98
rei0531 <poripuropiren12@gmail.com>
2020-06-15 12:55:12 +0900

    マップ画像変更

--- U-22Team2/LoadPic.cpp
+++ U-22Team2/LoadPic.cpp
@@ -3,7 +3,7 @@
 
 int LoadPictue(void) {		//‰æ‘œ‚Ì“Ç‚İ‚İ
 
-	if ((g_pic.Map = LoadGraph("image/ƒ}ƒbƒv.png")) == -1) return -1;
+	if ((g_pic.Map = LoadGraph("image/map.png")) == -1) return -1;
 
 	if (LoadDivGraph("image/mouse.png", 3, 3, 1, 200,100 , g_pic.Player) == -1) return -1;
 	
