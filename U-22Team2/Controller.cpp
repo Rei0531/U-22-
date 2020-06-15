0b4e57c0ed2d661298e42a998a4c9448d9de3a73
rei0531 <poripuropiren12@gmail.com>
2020-06-15 11:28:39 +0900

    移動用をdefainn ka

--- U-22Team2/Controller.cpp
+++ U-22Team2/Controller.cpp
@@ -1,6 +1,11 @@
 #include "Controller.h"
 #include "Map.h"
 
+/********************************
+*defineの定義
+********************************/
+#define PLYERX 8;		//プレイヤーの移動量
+
 //int PadInput = GetJoypadInputState(DX_INPUT_KEY_PAD1);
 //extern Controller g_Pad;
 //MapCoordinate MapC;
@@ -8,13 +13,13 @@
 void ControllerVlue(void) {         //プレイヤーの移動処理関数
     if ((GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_LEFT) != 0)     //コントローラーがあるときにちゃんと使えます
     {
-        g_MapC.X1 += 4;
-        g_MapC.X2 += 4;
+        g_MapC.X1 += PLYERX;
+        g_MapC.X2 += PLYERX;
     }
     if ((GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_RIGHT) != 0)
     {
-        g_MapC.X1 -= 4;
-        g_MapC.X2 -= 4;
+        g_MapC.X1 -= PLYERX;
+        g_MapC.X2 -= PLYERX;
     }
     //if (CheckHitKey(KEY_INPUT_RIGHT) != 0) {          //コントローラーない時はこの処理でキーボードの十字キーで操作
     //    g_MapC.X1 -= 8;
