277f4a2776fe1c664f94452713d8f7e49634f8fa
Re-nia <yuki-kyuzato-19f@stu.kbc.ac.jp>
2020-06-15 12:58:43 +0900

    player描画　(描画されず)

--- U-22Team2/Player.cpp
+++ U-22Team2/Player.cpp
@@ -5,7 +5,9 @@
 image g_Play;
 extern Controller g_Pad;
 int Player(void) {
-	//DrawRotaGraph(640, 600, 2.0, 0, g_Play.Player[0], TRUE, FALSE);//ネズミ画像の描画
+	/*g_Pad.MouseX = 800;
+	g_Pad.MouseX = 1000;*/
+	//DrawRotaGraph(640, 600, 2.0, 0, g_Play.Player[0], TRUE);//ネズミ画像の描画
 	DrawRotaGraph(g_Pad.MouseX, g_Pad.MouseY, 2.0, 0, g_Play.Player[0], TRUE, FALSE);//ネズミ画像の描画
 	return 0;
 }
