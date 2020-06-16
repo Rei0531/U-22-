31520e4749035f5d60c511f74ee669b888300995
Re-nia <yuki-kyuzato-19f@stu.kbc.ac.jp>
2020-06-16 12:14:06 +0900

    プレイヤー移動アニメーション途中

--- U-22Team2/Player.cpp
+++ U-22Team2/Player.cpp
@@ -7,14 +7,18 @@ extern Controller g_Pad;
 
 int Player(void) {
 	static int cnt = 0;	//アニメーション用カウント変数
+	static int MouseX = 640;	//座標X
+	static int MouseY = 645;	//座標Y
 
 	if ((GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_RIGHT || GetJoypadInputState(DX_INPUT_PAD1) & PAD_INPUT_LEFT) != 0)
 	{
-		DrawRotaGraph(640, 645, 1.0, 0, g_pic.Player[cnt++ / 10 % 2 + 1], TRUE, FALSE);//ネズミ画像の描画
+		//DrawRotaGraph(640, 645, 1.0, 0, g_pic.Player[cnt++ / 10 % 2 + 1], TRUE, FALSE);//ネズミ画像の描画
+		DrawRotaGraph(MouseX, MouseY, 1.0, 0, g_pic.Player[cnt++ / 10 % 2 + 1], TRUE, FALSE);//ネズミ画像の描画
 	}
 	else
 	{
-		DrawRotaGraph(640, 645, 1.0, 0, g_pic.Player[0], TRUE, FALSE);//ネズミ画像の描画
+		//DrawRotaGraph(640, 645, 1.0, 0, g_pic.Player[0], TRUE, FALSE);//ネズミ画像の描画
+		DrawRotaGraph(MouseX, MouseY, 1.0, 0, g_pic.Player[0], TRUE, FALSE);//ネズミ画像の描画
 	}
 	return 0;
 }
