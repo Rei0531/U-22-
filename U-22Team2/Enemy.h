7ef7edc25f4f7f55b414f509faabe368e4c435aa
rei0531 <poripuropiren12@gmail.com>
2020-06-18 14:04:25 +0900

    ゆうの変更点プッシュ

--- U-22Team2/Enemy.h
+++ U-22Team2/Enemy.h
@@ -1,3 +1,14 @@
 #pragma once
+#include"LoadPic.h"
+#include"constant.h"
 
+extern image g_pic;
+
+struct EnemyCoordinate {			//敵のデフォルトの座標
+public:
+	int X1 = MAP_WIDHT - 50;		//敵画像のx座標の左端 -50は画像の仮の幅
+	int X2 = MAP_WIDHT;				//敵画像のx座標の右端
+	int Y1 = 0;						//敵画像のy座標の上端
+	int Y2 = SCREEN_HEIGHT;			//敵画像のy座標の下端
+}; extern EnemyCoordinate g_EnmC;
 int Enemy(void);		//関数の定義
--- U-22Team2/LoadPic.cpp
+++ U-22Team2/LoadPic.cpp
@@ -1,11 +1,15 @@
 #include "LoadPic.h"
 #include "Map.h"
+#include"Enemy.h"
 
 int LoadPictue(void) {		//‰æ‘œ‚Ì“Ç‚İ‚İ
-
 	if ((g_pic.Map = LoadGraph("image/map.png")) == -1) return -1;
+	if (LoadDivGraph("image/mouse.png", 3, 3, 1, 200, 100, g_pic.Player) == -1) return -1;
 
-	if (LoadDivGraph("image/mouse.png", 3, 3, 1, 200,100 , g_pic.Player) == -1) return -1;
-	
+	if ((g_pic.Enemy[0] = LoadGraph("image/Enemy(‰¼).jpg")) == -1)return -1;
+	if ((g_pic.Enemy[1] = LoadGraph("image/Enemy(‰¼).jpg")) == -1)return -1;
+	if ((g_pic.Enemy[2] = LoadGraph("image/Enemy(‰¼).jpg")) == -1)return -1;
+	if ((g_pic.Enemy[3] = LoadGraph("image/Enemy(‰¼).jpg")) == -1)return -1;
+	if ((g_pic.Enemy[4] = LoadGraph("image/Enemy(‰¼).jpg")) == -1)return -1;
 	return 0;
 }
