fb14516013de6a0e083622a794940a3bb1a7d7d9
DRQE <https://github.com/DRQE/mamimumema>
2020-07-03 09:21:22 +0900

    コメント化してたやつを修正&タイトル画面微調整

--- U-22Team2/LoadPic.cpp
+++ U-22Team2/LoadPic.cpp
@@ -6,8 +6,8 @@ int LoadPictue(void) {		//âÊëúÇÃì«Ç›çûÇ›
 	if ((g_pic.Map = LoadGraph("image/Stage_Deffault.jpg")) == -1) return -1;
 	if (LoadDivGraph("image/Player.png", 5, 5, 1, 85, 179, g_pic.Player) == -1) return -1;
 
-	//if ((g_pic.Title = LoadGraph("image/Title(proto).png")) == -1) return -1;
-	//if (LoadDivGraph("image/All_Object.png", 10, 10, 1, 150, 150, g_pic.Object) == -1) return -1;
+	if ((g_pic.Title = LoadGraph("image/Title(proto).png")) == -1) return -1;
+	if (LoadDivGraph("image/All_Object.png", 10, 10, 1, 150, 150, g_pic.Object) == -1) return -1;
 
 
 	return 0;
--- U-22Team2/Title.cpp
+++ U-22Team2/Title.cpp
@@ -7,10 +7,10 @@ extern image g_pic;
 extern Controller g_Pad;
 
 int GameTitle(void) {
-	DrawRotaGraph(SCREEN_WIDHT / 2, SCREEN_HEIGHT / 2, 1.2, 0,g_pic.Title, TRUE, FALSE); //タイトル画像描画
+	DrawRotaGraph(SCREEN_WIDHT / 2, SCREEN_HEIGHT / 2, 1.4, 0,g_pic.Title, TRUE, FALSE); //タイトル画像描画
 	
 	if (g_Pad.KEY_B == TRUE) {
-		GameState = 4 ;
+		GameState = GAME_MAIN ;
 	}
 	return 0;
 }
