6375efe9a3babac321e51d0eecff09a7d0d0adb7
rei0531 <poripuropiren12@gmail.com>
2020-08-04 12:41:39 +0900

    ボタンの色がリセットされない不具合を修正

--- U-22Team2/CC_Switch.cpp
+++ U-22Team2/CC_Switch.cpp
@@ -4,13 +4,15 @@
 #include "Controller.h"
 #include "Player.h"
 #include "LoadSound.h"
+#include"Lock.h"
 
 extern Controller g_Pad;
 extern image g_pic;
 extern Player g_Player;
 extern Sound g_Snd;
+extern LockALL g_Lock;
 
-static int PushColor = 9;//押された色を引数から格納する変数
+int PushColor = 9;//押された色を引数から格納する変数
 
 int CC_Switch(int Color,int x,int y) {
 
@@ -24,6 +26,8 @@ int CC_Switch(int Color,int x,int y) {
 		PushColor = 9;
 		g_Player.PLAYER_RESET = FALSE;//リセットしたフラグをFALSEにし続ける
 	}
+
+	if (g_Lock.clearflg == TRUE)PushColor = 9;
 
 	Change(PushColor);
 	DrawExtendGraph(x, y, x+100, y+100, g_pic.C_Switch, TRUE);
