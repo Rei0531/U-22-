37102f27cbdb4fd165e6e17f30fd68be0b30d5fe
nishimetaiga <taiga-nishime-19f@stu.kbc.ac.jp>
2020-08-13 19:42:54 +0900

    ステージ２３を作りました。

--- U-22Team2/Stage23.cpp
+++ U-22Team2/Stage23.cpp
@@ -6,27 +6,56 @@
 #include "Draw_Door_Rotation.h"
 #include "Rotation_Box.h"
 #include "Menu.h"
+#include "Gimmick.h"
 
 //MapCoordinate g_MapC;
 extern MapCoordinate g_MapC;
 extern Player g_Player;
 extern DoorAll g_Door;
 extern LockALL g_Lock;
+extern GimmickAll gim;
 
 static bool InitFlag = TRUE;//Init関数を通っていいか判定変数/TRUEがいい/FALSEがダメ
+
+static int SwitchColor = 9;
 
 void Stage23Init() {
 	//プレイヤーの初期位置
 	//オブジェクトの初期位置を描く
-	g_Player.Interact = 20;//プレイヤーがインタラクトできる回数を10回に設定
+	g_Player.Interact = 10;//プレイヤーがインタラクトできる回数を10回に設定
 	InitFlag = FALSE;	//FALSEにして次TRUEになるまで通らないようにする
 
 	g_Player.x = 110;			//プレイヤー座標初期化
 	g_Player.y = 571;			//プレイヤー座標初期化
-	g_Player.NowColor = 0;		//プレイヤーの色初期化
+	g_Player.NowColor = YELLOW;		//プレイヤーの色初期化
 
 	g_Door.RotationNumber = 0;	//ローテーション初期化
 	g_Lock.Release = 0;			//鍵穴解除数初期化
+
+	SwitchColor = 9;//スイッチから―の初期化
+
+	gim.cheobj_flg = 0;			//変形するオブジェクトのフラグ
+	gim.cheobj_x = 800;
+	gim.cheobj_y = 670 - 225;
+	gim.cheobj_c = g_Player.NowColor;
+	gim.cheobj_ani = 126;
+
+
+	gim.g_OTSwitchFlag = 0;		//一度限りのスイッチフラグ初期化
+
+	gim.OTS_X1 = 1135;				//一度限りのスイッチのX/Y軸初期化
+	gim.OTS_Y1 = 618;
+	gim.OTS_X2 = gim.OTS_X1 + 50;
+	gim.OTS_Y2 = gim.OTS_Y1 + 50;
+	gim.OTS_WallX1 = 250;
+	gim.OTS_WallY1 = 0;
+	gim.OTS_WallX2 = 300;
+	gim.OTS_WallY2 = 370;
+	gim.OTS_C_WallX1 = 0;			//一度限りのスイッチで表れる壁のX/Y軸
+	gim.OTS_C_WallY1 = 0;
+	gim.OTS_C_WallX2 = 0;
+	gim.OTS_C_WallY2 = 669;
+
 
 	for (int i = 0; g_Lock.n[g_MapC.StageNumber - 1] > i; i++) {
 		g_Lock.color[g_MapC.StageNumber - 1][i] = g_Lock.colorback[g_MapC.StageNumber - 1][i];
@@ -34,7 +63,7 @@ void Stage23Init() {
 
 	//ドアの位置
 	g_Door.x = 1100;				//扉の左上のx座標
-	g_Door.y = 468;				//扉の左上のy座標
+	g_Door.y = 370 - 200;				//扉の左上のy座標
 	g_Door.w = g_Door.x + 100;	//横幅
 	g_Door.h = g_Door.y + 200;	//縦幅
 
@@ -48,13 +77,55 @@ int Stage23(void) {			//マップ画像の描画
 
 	DrawExtendGraph(g_MapC.X1, g_MapC.Y1, g_MapC.X2, g_MapC.Y2, g_pic.Map, TRUE);	//マップの描画
 
+
+
+
+	Change(NONCOLOR);
+	DrawBox(0, 370, 350, 400, GetColor(255, 255, 255), TRUE);
+	Change(RED);
+	DrawExtendGraph(0, 270, 100, 370, g_pic.Box, TRUE);	//1
+
+
+
+
+	Change(BLUE);
+	DrawExtendGraph(300, 568, 400, 668, g_pic.Box, TRUE);	//1
+	DrawExtendGraph(300, 468, 400, 568, g_pic.Box, TRUE);	//2
+
+
+		//色反映スイッチ_____________________
+	SwitchColor = CC_Switch(g_Player.NowColor, 200, 568);//一時変数に関数からの戻り値を格納する
+	Change(SwitchColor);
+	//色反映する箱________________________________________________
+
+	DrawExtendGraph(500, 568, 600, 668, g_pic.Box, TRUE);	//1
+	DrawExtendGraph(500, 468, 600, 568, g_pic.Box, TRUE);	//2
+
+
+	//箱の枠線__________________________________________
+	ColorReset();
+	DrawBox(500, 568, 600, 668, GetColor(1, 1, 1), FALSE);
+	DrawBox(500, 468, 600, 568, GetColor(1, 1, 1), FALSE);
+
+	Change(YELLOW);
+	DrawExtendGraph(400, 568, 500, 668, g_pic.Box, TRUE);
+
+	ChangeBlock();
+
+
+	Change(NONCOLOR);
+	DrawBox(1280 - 250, 370, 1280, 400, GetColor(255, 255, 255), TRUE);
+
+
 	Door();			//ステージゴール処理
 	Lock();
 
 
-	DoorRotationBox(2);
+	DoorRotationBox(3);
 
 	ColorReset();
+
+	OneTimeSwitch();
 
 	//ステージクリアした時、タイトル画面に戻ったとき
 	if (g_Lock.clearflg == TRUE || g_Player.InitFlag == TRUE) {
