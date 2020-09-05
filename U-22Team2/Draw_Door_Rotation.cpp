503f4593501aa89de26b7a2702ee7951531a5103
rei0531 <poripuropiren12@gmail.com>
2020-09-05 15:22:56 +0900

    ドアローテーションにアニメーションを追加しました

--- U-22Team2/Draw_Door_Rotation.cpp
+++ U-22Team2/Draw_Door_Rotation.cpp
@@ -14,22 +14,24 @@ extern LockALL g_Lock;
 extern Player g_Player;
 
 float Move_Rotation = 0.0,
-	  Rot_anicnt = 0.0;//回転のアニメ用の変数
+Rot_anicnt = 0.0,//回転のアニメ用の変数
+Rot_Circle = 0.0;//ローテーション用の変数
 
 //Rot_sum:ローテションす数
 void DoorRotationBox(int Rot_sum) {
 
 	Rot_anicnt -= 0.05;//矢印を回転させる
 
-	int
-		RotationNum = Rot_sum,//ローテーションする数
-		X_Size = 100 / RotationNum;//ローテーションのボックスの横のサイズ
-
 	double RotationAngle = (PI*2.0) / (double)Rot_sum;
 
 	if (g_Door.Picupflg == TRUE) {//ドアのローテーションを進めるフラグがTRUEの時
+		Rot_Circle = Move_Rotation;//ローテションする開始位置を取得
 		Move_Rotation += (PI * 2.0) / (double)Rot_sum;
 		g_Door.Picupflg = FALSE;
+	}
+	//ローテションする処理
+	if (Move_Rotation > Rot_Circle) {
+		Rot_Circle += 0.1;
 	}
 
 	//ステージクリアした時、タイトル画面に戻ったとき,ステージリセットされたとき
@@ -41,23 +43,19 @@ void DoorRotationBox(int Rot_sum) {
 	//ローテーションする数に合わせて描画する数を決める
 	if (Rot_sum >= 1) {
 		Change2(g_Door.Rotation[g_MapC.StageNumber - 1][0]);
-		//DrawBox(g_Door.x, g_Door.y - 30, g_Door.x + X_Size, g_Door.y, GetColor(255, 255, 255), TRUE);
-		DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 1.0, RotationAngle*4.0 - Move_Rotation, g_pic.Door_Rotation[Rot_sum - 1], TRUE, FALSE);
+		DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 1.0, RotationAngle * 4.0 - Rot_Circle, g_pic.Door_Rotation[Rot_sum - 1], TRUE, FALSE);
 	}
 	if (Rot_sum >= 2) {
 		Change2(g_Door.Rotation[g_MapC.StageNumber - 1][1]);
-		//DrawBox(g_Door.x + X_Size, g_Door.y - 30, g_Door.x + X_Size * 2, g_Door.y, GetColor(255, 255, 255), TRUE);
-		DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 1.0, RotationAngle * 5.0 - Move_Rotation, g_pic.Door_Rotation[Rot_sum - 1], TRUE, FALSE);
+		DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 1.0, RotationAngle * 5.0 - Rot_Circle, g_pic.Door_Rotation[Rot_sum - 1], TRUE, FALSE);
 	}
 	if (Rot_sum >= 3) {
 		Change2(g_Door.Rotation[g_MapC.StageNumber - 1][2]);
-		//DrawBox(g_Door.x + X_Size * 2, g_Door.y - 30, g_Door.x + X_Size * 3, g_Door.y, GetColor(255, 255, 255), TRUE);
-		DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 1.0, RotationAngle * 6.0 - Move_Rotation, g_pic.Door_Rotation[Rot_sum - 1], TRUE, FALSE);
+		DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 1.0, RotationAngle * 6.0 - Rot_Circle, g_pic.Door_Rotation[Rot_sum - 1], TRUE, FALSE);
 	}
 	if (Rot_sum >= 4) {
 		Change2(g_Door.Rotation[g_MapC.StageNumber - 1][3]);
-		//DrawBox(g_Door.x + X_Size * 3, g_Door.y - 30, g_Door.x + X_Size * 4, g_Door.y, GetColor(255, 255, 255), TRUE);
-		DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 1.0, RotationAngle * 7.0 - Move_Rotation, g_pic.Door_Rotation[Rot_sum - 1], TRUE, FALSE);
+		DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 1.0, RotationAngle * 7.0 - Rot_Circle, g_pic.Door_Rotation[Rot_sum - 1], TRUE, FALSE);
 	}
 	DrawRotaGraph(g_Door.x + 50, g_Door.y - 30, 0.7, Rot_anicnt, g_pic.Door_Rot_Yajirusi, TRUE, FALSE);
 }
